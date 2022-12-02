8939f69b97b0e8c05df3c85675cd7cc7f9bcc903
Rudenko0339 <rudenko0339@gmail.com>
2022-12-02 22:01:39 +0200

    fix task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+const colorizer = {
+  intervalId: null,
+  start() {
+    this.intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+      refs.startBtn.disabled = true;
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    refs.startBtn.disabled = false;
+  },
+};
+
+function onStartClick() {
+  colorizer.start();
+}
+
+function onStopClick() {
+  colorizer.stop();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
